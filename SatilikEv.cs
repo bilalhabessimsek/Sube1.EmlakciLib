5e81a827b0f806645b6d6fe0bb64b52abc0c03be
bilal <bilal@CANOWAR>
2025-05-14 04:56:49 +0300

    Add project files.

---
+++ SatilikEv.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sube1.EmlakciLib
+{
+    public class SatilikEv : Ev
+    {
+        public SatilikEv()
+        {
+
+        }
+
+        public SatilikEv(double satisfiyat, int odasayisi, string semt, int katno, double alan) : base(odasayisi, katno, alan, semt)
+        {
+            Satisfiyat = satisfiyat;
+        }
+
+        public double Satisfiyat { get; set; }
+
+        //public new string EvBilgileri()
+        //{
+        //    return $"{base.EvBilgileri()}\nSatış Fiyat:{this.Satisfiyat}";
+        //}
+
+        public override string EvBilgileri()
+        {
+            return $"{base.EvBilgileri()}\nSatış Fiyat:{this.Satisfiyat}";
+        }
+    }
+}
+
